b55472466b7d484890aaecdf19c051f69bf6ebb0
Usman Ali <usman.okara@hotmail.com>
2021-11-17 18:35:27 +0500

    code

--- lib/flutter_key_hash.dart
+++ lib/flutter_key_hash.dart
@@ -6,6 +6,7 @@ class FlutterKeyHash {
   static const MethodChannel _channel = MethodChannel('flutter_key_hash');
 
   static Future<String?> get getFlutterKeyHash async {
+    //code
     final String? version = await _channel.invokeMethod('getFlutterKeyHash');
     return version;
   }
